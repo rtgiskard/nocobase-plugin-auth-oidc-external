519c7f7e5be312f67350d3d1aeef2c37301b9fbf
giskard <rtgiskard@gmail.com>
2026-08-08 15:58:52 +0800

    Write canonical plugin entrypoints

--- scripts/write-entrypoints.mjs
+++ scripts/write-entrypoints.mjs
@@ -1,6 +1,7 @@
 import { copyFileSync, writeFileSync } from 'node:fs';
 
-copyFileSync('dist/server/index.js', 'server.js');
 copyFileSync('dist/server/index.d.ts', 'server.d.ts');
+writeFileSync('server.js', "module.exports = require('./dist/server/index.js');\n");
+writeFileSync('client.js', "module.exports = require('./dist/client/index.js');\n");
 writeFileSync('client-v2.js', "module.exports = require('./dist/client-v2/index.js');\n");
 writeFileSync('client-v2.d.ts', "export * from './dist/client-v2';\nexport { default } from './dist/client-v2';\n");
